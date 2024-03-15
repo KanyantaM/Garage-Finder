85777e6affbba7c12a6e486bfc322ddad2aa8f7a
KanyantaM <kanyanta.1makasa@gmail.com>
2024-03-15 22:02:59 +0200

    finishing the UI for the appointments screen

--- lib/widgets/booking_item.dart
+++ lib/widgets/booking_item.dart
@@ -5,33 +5,38 @@ class BookingData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: ShapeDecoration(
-            image: const DecorationImage(
-              image: AssetImage("assets/images/dummy.png"),
-              fit: BoxFit.fill,
-            ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-        ),
-        const SizedBox(
-          child: Text(
-            'Auto car centre \nThursday 18th of May 11:00AM',
-            style: TextStyle(
-              color: Color(0xFF4D4C4C),
-              fontSize: 20,
-              fontFamily: '?????',
-              fontWeight: FontWeight.w400,
-              height: 0,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Container(
+            width: 344,
+            height: 82,
+            decoration: ShapeDecoration(
+              image: const DecorationImage(
+                image: AssetImage("assets/images/dummy.png"),
+                fit: BoxFit.fill,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
             ),
           ),
-        ),
-        const Divider()
-      ],
+          const SizedBox(
+            child: Text(
+              'Auto car centre \nThursday 18th of May 11:00AM',
+              style: TextStyle(
+                color: Color(0xFF4D4C4C),
+                fontSize: 20,
+                fontFamily: '?????',
+                fontWeight: FontWeight.w400,
+                height: 0,
+              ),
+            ),
+          ),
+          const Divider()
+        ],
+      ),
     );
   }
 }
