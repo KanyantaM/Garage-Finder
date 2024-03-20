8b0b0b3b831969dcca0931b342a9dcf5daeff752
KanyantaM <kanyanta.1makasa@gmail.com>
2024-03-21 00:23:24 +0200

    finalizing the repo

--- packages/cloud_storage_garage_api/lib/src/firestore_storage_garage.dart
+++ packages/cloud_storage_garage_api/lib/src/firestore_storage_garage.dart
@@ -3,6 +3,8 @@ import 'package:garage_api/garage_api.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
 
+///This implementation uses firebase for the backend
+///the rest of the location features are done using free on device services from the imported packages
 class FirestoreGarageApi implements GarageApi {
   final CollectionReference _garagesCollection =
       FirebaseFirestore.instance.collection('garages');
@@ -29,37 +31,37 @@ class FirestoreGarageApi implements GarageApi {
     }
     await _garagesCollection.doc(id).delete();
   }
-  
+
   @override
-  Future<List<Garage>> arrangeGarageByLocation(String? postcode, double? lat, double? lng, List<Garage> garages) async{
+  Future<List<Garage>> arrangeGarageByLocation(
+      String? postcode, double? lat, double? lng, List<Garage> garages) async {
     double startLatitude = 0;
     double startLongitude = 0;
     if (postcode != null) {
-    List<Location> locations = await locationFromAddress(postcode);
-    if (locations.isNotEmpty) {
-      startLatitude = locations.first.latitude;
-      startLongitude = locations.first.longitude;
-    } else {
-      // Handle case where postcode couldn't be found
-      // For example, show an error message or fallback to default coordinates
+      List<Location> locations = await locationFromAddress(postcode);
+      if (locations.isNotEmpty) {
+        startLatitude = locations.first.latitude;
+        startLongitude = locations.first.longitude;
+      } else {
+        // Handle case where postcode couldn't be found
+        // For example, show an error message or fallback to default coordinates
+      }
+    } else if (lat != null && lng != null) {
+      startLatitude = lat;
+      startLongitude = lng;
     }
-  } else if (lat != null && lng != null) {
-    startLatitude = lat;
-    startLongitude = lng;
+
+    garages.sort((a, b) {
+      // Calculate the difference from the targetNumber in absolute terms
+      double diffA = Geolocator.distanceBetween(
+          startLatitude, startLongitude, a.lat, a.lng);
+      double diffB = Geolocator.distanceBetween(
+          startLatitude, startLongitude, b.lat, b.lng);
+
+      // Compare the absolute differences
+      return diffA.compareTo(diffB);
+    });
+
+    return garages;
   }
-
-
-  garages.sort((a, b) {
-    // Calculate the difference from the targetNumber in absolute terms
-    double diffA = Geolocator.distanceBetween(startLatitude, startLongitude, a.lat, a.lng);
-    double diffB = Geolocator.distanceBetween(startLatitude, startLongitude, b.lat, b.lng);
-
-    // Compare the absolute differences
-    return diffA.compareTo(diffB);
-  });
-
-  return garages;
-  }
-  
-
 }
--- packages/garage_repository/lib/garage_repository.dart
+++ packages/garage_repository/lib/garage_repository.dart
@@ -1,4 +1,9 @@
-export './garage_repository.dart';
+library garage_repository;
+
+export 'package:garage_api/garage_api.dart' show Garage;
+export 'src/garage_repo.dart';
+
+
 
 /// A Calculator.
 class Calculator {
