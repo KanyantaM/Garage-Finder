e00dcae7d62e694dc8f5607fb38ebc01a97be86f
KanyantaM <kanyanta.1makasa@gmail.com>
2024-03-24 23:10:23 +0200

    adding firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:cloud_storage_garage_api/cloud_storage_garage_api.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fixtex/bootstrap.dart';
+import 'package:fixtex/firebase_options.dart';
 import 'package:flutter/widgets.dart';
 
 Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   WidgetsFlutterBinding.ensureInitialized();
 
   final CloudGarageApi garageApi = CloudGarageApi();
